610d01d1056c066b96446de8fac61c23825d13fc
unknown <pangaruystanley@gmail.com>
2023-01-23 13:57:01 +0800

    implemented insertion sort

---
+++ insertion/insertion.py
@@ -0,0 +1,18 @@
+
+def insertionSort(nums):
+
+    for step in range(1, len(nums)):
+        key = nums[step]
+        j = step - 1
+
+        while j >= 0 and key < nums[j]:
+            nums[j+1] = nums[j]
+            j = j-1
+        
+        nums[j+1] = key
+        print(nums)
+
+
+nums = [46, 4, 49, 52, 19, 90, 2, 81, 38, 98]
+print(nums)
+insertionSort(nums)
