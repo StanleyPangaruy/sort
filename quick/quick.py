9b8fc5cca4938cb971402dc4c0b9f32c7a548876
unknown <pangaruystanley@gmail.com>
2023-01-23 13:01:53 +0800

    implemented selection array

---
+++ quick/quick.py
@@ -0,0 +1,18 @@
+def sort(nums):
+
+    for i in range(9):
+        minpos = i
+        for j in range(i,10):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+        print(nums)
+
+nums = [46, 4, 49, 52, 19, 90, 2, 81, 38, 98]
+sort(nums)
+
+print(nums)
